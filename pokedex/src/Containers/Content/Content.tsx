b0f058530d6dd40f663a23f98c9332a588005bf4
Shai Dzindzihashvili <sdzindzihashvi@student.tgm.ac.at>
2021-02-23 11:49:18 +0100

    Set up router to go to "/pokemon/:id"

--- pokedex/src/Containers/Content/Content.tsx
+++ pokedex/src/Containers/Content/Content.tsx
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
+import { 
+    Route, 
+    BrowserRouter as Router,
+    Switch,
+    RouteComponentProps,
+    
+} from 'react-router-dom';
+import DetailView from '../DetailView/DetailView';
 import Onboarding from '../Onboarding/Onboarding';
 import Pokedex from '../Pokedex/Pokedex';
 
 class Content extends Component {
+
     render() {
         return (
-            <div>
-                <Onboarding />
-                <Pokedex />
-            </div>
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <Onboarding />
+                        <Pokedex />
+                    </Route>
+                    <Route path="/pokemon/:id" render={(props) => <DetailView {...props} />} />
+                </Switch>
+            </Router>
         );
     }
 }
--- pokedex/src/Containers/DetailView/DetailView.tsx
+++ pokedex/src/Containers/DetailView/DetailView.tsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 import styles from "./DetailView.module.css"
-import {Route} from "react-router-dom"
+import {Route, RouteComponentProps, Switch} from "react-router-dom"
 
-export default class DetailView extends Component {
+interface HomeRouterProps {
+    id: string;   // This one is coming from the router
+}
+
+class DetailView extends Component<RouteComponentProps<HomeRouterProps>, {}> {
     render() {
         return (
             <div>
-                
+                {this.props.match.params.id}
             </div>
         )
     }
 }
+
+export default DetailView
